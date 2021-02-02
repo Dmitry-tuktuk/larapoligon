7049209ccc2211ec2c34188423d2bf7a591435a9
Dmitry-tuktuk <content.manager@gmail.com>
2021-02-02 17:11:49 +0200

    Перенос папки routes из старого проекта"

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +14,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['prefix'=>'digging_deeper'], function () {
+    Route::get('collections', 'DiggingDeeperController@collections')
+        ->name('digging_deeper.collections');
+});
+
+Route::group(['namespace'=>'Blog','prefix'=>'blog'], function(){
+    Route::resource('posts', 'PostController')->names('blog.posts');
+});
+
+//Админка Блога
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    //BlogCategory
+    $methods = ['index', 'edit', 'update', 'create', 'store'];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+
+    //BlogPosts
+    Route:: resource('posts', 'PostController')
+        ->except(['show'])
+        ->names('blog.admin.posts');
+});
